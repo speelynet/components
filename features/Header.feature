22dc53da48dc5bcef5cd1e375c3bcc3ef9950e96
Noah Friedman <noahfriedman2@gmail.com>
2021-07-15 13:01:53 -0400

    Remove Cucumber test for Header component because it is unnecessary, add Parcel as a package dependency

--- features/Header.feature
+++
@@ -1,7 +0,0 @@
-Feature: Header component
-
-  @wip
-  Scenario: content
-    Given I render a <custom-header> component
-    Then I should see "Welcome to SpeelyNet!"
-    And I should see a sub-heading from subheadings.json
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,13 +1,2 @@
 import {Given, When, Then} from "@cucumber/cucumber";
 import assert from "assert";
-
-Given("I render a(n) <{word}> component", function(word) {
-  document.body.innerHTML = "";
-  this.component = document.createElement(word);
-  this.shadowRoot = this.component.shadowRoot;
-  document.body.appendChild(this.component);
-});
-
-Then("I should see {string}", function(string) {
-  assert.match(this.shadowRoot.textContent, new RegExp(string, "g"));
-});
