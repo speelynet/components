02582f6c07aa7ff14f421cbac3c72a6a6cd929ba
Noah Friedman <noahfriedman2@gmail.com>
2021-09-02 12:50:29 -0400

    Implement ToggleSwitch component visually

--- src/ToggleSwitch.js
+++ src/ToggleSwitch.js
@@ -3,10 +3,56 @@ template.innerHTML = `
 <style>
     input {
         visibility: hidden;
+        position: relative;
+    }
+    
+    input:before, input:after {
+        content: "";
+        visibility: visible;
+        position: absolute;
+        transition: 0.4s;
     }
     
     input:before {
-        content: "";
+        top: 0;
+        left: 0;
+        width: 9.75vmin;
+        height: 5.25vmin;
+        border-radius: 5.25vmin;
+        --dark: var(--dark-mode) #3e3e3e;
+        background-color: var(--dark, white);
+    }
+    
+    input:after  {
+        top: 0.25vmin;
+        left: 0.25vmin;
+        width: 4.75vmin;
+        height: 4.75vmin;
+        border-radius: 4.75vmin;
+        --dark: var(--dark-mode) white;
+        background-color: var(--dark, #3e3e3e);
+    }
+    
+    input:checked:after {
+        transform: translateX(4.35vmin);
+    }
+    
+    @media (min-width: 800px), (min-height: 800px) and (orientation: portrait) {
+        input:before {
+            width: 5.5vmin;
+            height: 3vmin;
+            border-radius: 3vmin;
+        }
+        input:after {
+            top: 0.14vmin;
+            left: 0.14vmin;
+            width: 2.75vmin;
+            height: 2.75vmin;
+            border-radius: 2.75vmin;
+        }
+        input:checked:after {
+            transform: translateX(2.5vmin);
+        }
     }
 </style>
 <input type="checkbox" />
