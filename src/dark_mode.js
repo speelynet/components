cbe83f9628cd265c6a7d93ee9f026b6b8cbbc225
Noah Friedman <noahfriedman2@gmail.com>
2021-09-27 11:56:11 -0400

    Fix issues with cookies

--- src/dark_mode.js
+++ src/dark_mode.js
@@ -1,12 +1,12 @@
 // Check browser cookies for stored dark mode setting
-const cookie = (/dark_mode=([^;]*);/.exec(document.cookie) ?? ["", ""])[1].trim();
+const cookie = (/dark_mode=([^;]*)/.exec(document.cookie) ?? ["", ""])[1].trim();
 
 let darkMode;
 if (cookie !== "true" && cookie !== "false") { // If no cookie is found, assume preferred colour scheme
   darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
   document.cookie = `dark_mode=${darkMode};`;
 } else { // If a cookie is found, use the value in the cookie.
-  darkMode = Boolean(cookie);
+  darkMode = cookie === "true";
 }
 
 // Add dark mode property to document element
